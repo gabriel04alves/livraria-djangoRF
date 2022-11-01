aeffbc60efd92f24dbebc65b4923d8cca766b0e0
gabriel04alves <gabriel52.alves@gmail.com>
2022-11-01 17:39:12 -0300

    Aula 12 e 13 - Permissões de acesso /usando do DRF

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from rest_framework.viewsets import ModelViewSet
-
+# from rest_framework.permissions import IsAuthenticated
 from core.models import Categoria, Editora, Livro, Autor
 from core.serializers import CategoriaSerializer, EditoraSerializer, LivroSerializer, LivroDetailSerializer, AutorSerializer
 
@@ -7,6 +7,7 @@ from core.serializers import CategoriaSerializer, EditoraSerializer, LivroSerial
 class CategoriaViewSet(ModelViewSet):
     queryset = Categoria.objects.all()
     serializer_class = CategoriaSerializer
+    # permission_classes = [IsAuthenticated]
 
 
 class EditoraViewSet(ModelViewSet):
