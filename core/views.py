a90c5e5ad9c77a10cba78c806b5a27169a7220b7
gabriel04alves <gabriel52.alves@gmail.com>
2022-11-02 16:13:44 -0300

    Aula 17 (sem erro) - Add campos ao usuário padrão

--- core/views.py
+++ core/views.py
@@ -1,3 +1,3 @@
 from rest_framework.viewsets import ModelViewSet
-from core.models import Categoria, Editora, Livro, Autor
+from core.models import Categoria, Editora, Livro, Autor, Usuario
 from core.serializers import CategoriaSerializer, EditoraSerializer, LivroSerializer, LivroDetailSerializer, AutorSerializer
